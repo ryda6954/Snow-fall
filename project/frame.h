6bdd59f93c7943c073f80d905a8b013b7d63a4b8
mayaam <jeki9042@colorado.edu>
2016-04-28 14:11:55 -0600

    Update frame.h

--- project/frame.h
+++ project/frame.h
@@ -1,8 +1,8 @@
 #ifndef FRAME_H
 #define FRAME_H
 
-const int width = 4;
-const int height = 4;
+const int width = 30;
+const int height = 10;
 
 class Frame{
 public:
@@ -14,6 +14,8 @@ public:
 	void createNextFrame();
 	void simulate();
 	void animate();
+	void random();
+	void snow();
 };
 
 
